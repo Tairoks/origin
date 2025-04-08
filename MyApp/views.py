1a105272f26b7f0e2e027b16672f1c1b307e2e13
Tairoks <vlad-hunter.ru@bk.ru>
2025-04-08 23:29:27 +0300

    add_readme_and_requiements

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Topic
 
+
 def index (request):
     """Home view"""
     return render(request, 'myapp/index.html')
@@ -10,3 +11,10 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'MyApp/topics.html', context)
+
+def topic(request, topic_id):
+    """View topic"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'MyApp/topic.html', context)
