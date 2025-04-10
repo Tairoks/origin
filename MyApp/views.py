c4f8ed5e78232ea976481a02f61474724a182332
Tairoks <vlad-hunter.ru@bk.ru>
2025-04-10 23:24:05 +0300

    add_form_for_topics

--- MyApp/views.py
+++ MyApp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 
 def index (request):
@@ -18,3 +21,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'MyApp/topic.html', context)
+
+def new_topic(request):
+    """Create new topic"""
+    if request.method != 'POST':
+        # Data not sent, empty form created
+        form = TopicForm()
+    else:
+        # POST data sent, process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('MyApp:topics')
+    context = {'form': form}
+    return render(request, 'myapp/new_topic.html', context)
